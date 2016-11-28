a4648c855bebb956041820b52cd31d2147c04ffd
Grigory Bordyugov <gregbg@gmail.com>
2016-11-28 17:07:14 +0100

    continued chapter 4

--- chapter4.scm
+++ chapter4.scm
@@ -16,10 +16,34 @@
 
 (+ 46 12)
 
-(define plus
+(define add
   (lambda (n m)
     (cond
       ((zero? m) n)
-      (else (add1 (plus n (sub1 m)))))))
+      (else (add (add1 n) (sub1 m))))))
 
-(o+ 3 4)
+(add 3 4)
+
+(define sub
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (sub (sub1 n) (sub1 m))))))
+
+(sub 4 3)
+
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (add (car tup) (addtup (cdr tup)))))))
+
+(addtup '(1 2 3 4))
+
+(define mul
+  (lambda (x y)
+    (cond
+      ((eq? y 0) 0)
+      (else (add x (mul x (sub1 y)))))))
+
+(mul 3 5)
