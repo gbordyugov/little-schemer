8a7b6e0a4b389219c5747f6b9fa37f8dd895a70f
Grigory Bordyugov <gregbg@gmail.com>
2016-12-02 16:31:13 +0100

    finished chapter 8

--- chapter8.scm
+++ chapter8.scm
@@ -203,9 +203,11 @@
   (cond
     ((null? lat) (col '() '()))
     ((eq? (car lat) a)
-     (multirember&co a (cdr lat) (lambda (newlat seen)
-                                   (col newlat (cons (car lat) seen)))))
-    (else (multirember&co a (cdr lat) (lambda (newlat seen)
+     (multirember&co a (cdr lat)
+                     (lambda (newlat seen)
+                       (col newlat (cons (car lat) seen)))))
+    (else (multirember&co a (cdr lat)
+                          (lambda (newlat seen)
                             (col (cons (car lat) newlat) seen))))))
 
 (define (a-friend x y)
@@ -219,3 +221,83 @@
   (a-friend newlat (cons 'tuna seen)))
 
 ;; continue reading by recapping from page 137 on
+
+(define (multiinsertL new old lat)
+  (cond
+    ((null? lat) '())
+    ((eq? (car lat) old)
+     (cons new (cons old (multiinsertL new old (cdr lat)))))
+    (else (cons (car lat) (multiinsertL new old (cdr lat))))))
+
+(multiinsertL 'new 'old '(old old old new old))
+
+(define (multiinsertLR new oldL oldR lat)
+  (cond
+    ((null? lat) '())
+    ((eq? (car lat) oldL)
+     (cons new (cons oldL (multiinsertLR new oldL oldR (cdr lat)))))
+    ((eq? (car lat) oldR)
+     (cons oldR (cons new (multiinsertLR new oldL oldR (cdr lat)))))
+    (else (cons (car lat) (multiinsertLR new oldL oldR (cdr lat))))))
+
+(multiinsertLR 'new 'oldL 'oldR '(oldR oldL old new oldL))
+
+(define (multiinsertLR&co new oldL oldR lat col)
+  (cond
+    ((null? lat) (col '() 0 0))
+    ((eq? (car lat) oldL)
+     (multiinsertLR&co new oldL oldR (cdr lat)
+                       (lambda (la l r)
+                         (col (cons new (cons oldL la)) (+ 1 l) r))))
+    ((eq? (car lat) oldR)
+     (multiinsertLR&co new oldL oldR (cdr lat)
+                       (lambda (la l r)
+                         (col (cons oldR (cons new la)) l (+ 1 r)))))
+    (else
+     (multiinsertLR&co new oldL oldR (cdr lat)
+                       (lambda (la l r)
+                         (col (cons (car lat) la) l r))))))
+
+(multiinsertLR&co 'cranberries 'fish 'chips '(fish chips)
+                  (lambda (l x y)
+                    (list l x y)))
+
+(define (evens-only* s)
+  (cond
+    ((null? s) '())
+    ((atom? (car s))
+     (if (even? (car s))
+       (cons (car s) (evens-only* (cdr s)))
+       (evens-only* (cdr s))))
+    (else (cons (evens-only* (car s)) (evens-only* (cdr s))))))
+
+(evens-only* '(1 2 (1 2 3 (1 2 3 (1 2) 3) (3))))
+
+(evens-only* '((9 1 2 8) 3 10 ((9 9) 7 6) 2))
+
+;;
+;; this one is tough
+;;
+(define (evens-only*&co x col)
+  (cond
+    ((null? x) (col '() 0 1))
+    ((atom? (car x))
+     (if (even? (car x))
+       (evens-only*&co (cdr x) (lambda (l s p)
+                                 (col (cons (car x) l) s (* p (car x)))))
+       (evens-only*&co (cdr x) (lambda (l s p)
+                                 (col l (+ s (car x)) p)))))
+    (else (evens-only*&co (car x)
+                          (lambda (al as ap)
+                            (evens-only*&co (cdr x)
+                                            (lambda (dl ds dp)
+                                              (col (cons al dl)
+                                                   (+ ap dp)
+                                                   (* as ds)))))))))
+
+(evens-only*&co '(2) (lambda (l p s) (list l p s)))
+
+(evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7 6) 2) (lambda (l p s)
+                                                  (list l p s)))
+
+
